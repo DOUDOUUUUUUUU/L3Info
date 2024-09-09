8d1a7f5be81deb1ec1cfcfe3b0d9cfba6397144a
DOUDOUUUUUUUU <dumenicu.franceschi2b@gmail.com>
2024-09-09 13:41:27 +0200

    somes changes

---
+++ Python/A3/ex1/main.py
@@ -0,0 +1,34 @@
+
+def full_name(arg:str)->str:
+    try:
+        new_arg = ""
+        i = 0
+        while arg[i] != " ":
+            new_arg += arg[i].upper()
+            i += 1
+        new_arg += arg[i:]
+        return new_arg
+    except:
+        raise NameError
+
+
+def is_mail(arg:str)->tuple[int, int]:
+    i = 0
+    if ('@' not in arg):
+        return (0, 2)
+    elif ('@' or '.' == arg[0]):
+        return (0, 1)
+    elif ('corse.' not in arg):
+        return (0, 4)
+    elif ('@univ-corse' not in arg):
+        return (0, 3)
+    return (1, 0)
+
+
+
+
+if (__name__ == "__main__"):
+    #print(full_name("qwerty"))
+    #print(is_mail("test@univ-corse.fr"))
+    
+    pass
